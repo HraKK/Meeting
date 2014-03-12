031b3f5123b8f9f0b806cd0158f1b8b08b748215
admin1 <admin1@B300-29>
2014-03-12 16:40:11 +0200

    models and controllers

---
+++ app/controllers/EventController.php
@@ -0,0 +1,11 @@
+<?php
+
+class EventController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+        echo "<h1>Hello!</h1>";
+    }
+
+}
---
+++ app/controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+class UserController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+        echo "<h1>Hello!</h1>";
+    }
+
+}
