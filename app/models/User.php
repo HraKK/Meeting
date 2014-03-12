0ca03bb90faf57c20c6603313641f1e07da134e1
admin1 <admin1@B300-29>
2014-03-12 18:13:43 +0200

    db connection and testing fetch

--- app/models/User.php
+++ app/models/User.php
@@ -4,12 +4,12 @@ class User extends \Phalcon\Mvc\Model
 {
     public function test() 
     {
-//        $con = $this->di->get('mydb_con');
         $con = \Phalcon\DI::getDefault()->get('mydb_con');
-        $sql = "select * from users limit 1";
-        $res = mysqli_query($con,$sql);
-        $row = mysqli_fetch_assoc($res);
-        echo '1'.$row['my_field'].'2';
+        $result = $con->query("SELECT * FROM users ORDER BY name");
+        $result->setFetchMode(Phalcon\Db::FETCH_OBJ);
+        while ($robot = $result->fetch()) {
+            echo $robot->name;
+        }
     }
     
 }
