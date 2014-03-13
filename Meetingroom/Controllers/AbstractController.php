f6a0623cc62343d919795c94924079d6585b1f35
admin1 <admin1@B300-29>
2014-03-13 13:24:06 +0200

    db connection

--- Meetingroom/Controllers/AbstractController.php
+++ Meetingroom/Controllers/AbstractController.php
@@ -4,5 +4,12 @@ namespace Meetingroom\Controllers;
 
 abstract class AbstractController extends \Phalcon\Mvc\Controller
 {
+    protected  $db;
+    
     abstract public function indexAction();
+    
+    public function beforeExecuteRoute($dispatcher)
+    {
+        $this->db = \Phalcon\DI::getDefault()->get('mydb_con');
+    }
 }
--- Meetingroom/Models/User.php
+++ Meetingroom/Models/User.php
@@ -6,8 +6,7 @@ class User extends \Phalcon\Mvc\Model
 {
     public function test() 
     {
-        $con = \Phalcon\DI::getDefault()->get('mydb_con');
-        $result = $con->query("SELECT * FROM users ORDER BY name");
+        $result = $this->db->query("SELECT * FROM users ORDER BY name");
         $result->setFetchMode(\Phalcon\Db::FETCH_OBJ);
         while ($robot = $result->fetch()) {
             echo $robot->name;
