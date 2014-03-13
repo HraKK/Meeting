449ea59a00faa26092a0f1d6061aaf3948ad8d9a
bogi <bogi@syneforge.com>
2014-03-13 18:07:03 +0200

    added system toolbar; hide "Month" view; fixed color of Add/Edit Event form header

--- public/js/calendar/CalendarPanel.js
+++ public/js/calendar/CalendarPanel.js
@@ -40,7 +40,7 @@ Ext.define('Ext.calendar.CalendarPanel', {
      * If the day and week views are both hidden, the month view will show by default even if
      * this config is false.
      */
-    showMonthView: true,
+    showMonthView: false,
     /**
      * @cfg {Boolean} showNavBar
      * True to display the calendar navigation toolbar, false to hide it (defaults to true). Note that
