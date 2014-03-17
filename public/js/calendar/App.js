872bdd77f7e9fee82a092d4469ba83a57dd476aa
bogi <bogi@syneforge.com>
2014-03-17 16:58:28 +0200

    fixed drag zones

--- public/js/calendar/App.js
+++ public/js/calendar/App.js
@@ -208,117 +208,126 @@ Ext.define('Ext.calendar.App', {
                                 ]
                             },
                             {
-                                xtype: 'calendarpanel',
-                                eventStore: this.eventStore,
-                                calendarStore: this.calendarStore,
-                                border: false,
-                                bodyStyle: {
-                                    border: false
-                                },
-                                id: 'app-calendar',
+                                xtype: 'container',
                                 region: 'center',
-
-                                monthViewCfg: {
-                                    showHeader: true,
-                                    showWeekLinks: true,
-                                    showWeekNumbers: true
+                                border: false,
+                                style: {
+                                    background: '#fff'
                                 },
+                                items: [
+                                    {
+                                        xtype: 'calendarpanel',
+                                        id: 'app-calendar',
+                                        height: 549,
+                                        eventStore: this.eventStore,
+                                        calendarStore: this.calendarStore,
+                                        border: false,
+                                        bodyStyle: {
+                                            border: false
+                                        },
+                                        monthViewCfg: {
+                                            showHeader: true,
+                                            showWeekLinks: true,
+                                            showWeekNumbers: true
+                                        },
 
-                                listeners: {
-                                    'eventclick': {
-                                        fn: function(vw, rec, el) {
-                                            this.showEditWindow(rec, el);
-                                        },
-                                        scope: this
-                                    },
-                                    'eventover': function(vw, rec, el) {
-                                        //console.log('Entered evt rec='+rec.data.Title+', view='+ vw.id +', el='+el.id);
-                                    },
-                                    'eventout': function(vw, rec, el) {
-                                        //console.log('Leaving evt rec='+rec.data.Title+', view='+ vw.id +', el='+el.id);
-                                    },
-                                    'eventadd': {
-                                        fn: function(cp, rec) {
-                                            this.showMsg('Event ' + rec.data.Title + ' was added');
-                                        },
-                                        scope: this
-                                    },
-                                    'eventupdate': {
-                                        fn: function(cp, rec) {
-                                            this.showMsg('Event ' + rec.data.Title + ' was updated');
-                                        },
-                                        scope: this
-                                    },
-                                    'eventcancel': {
-                                        fn: function(cp, rec) {
-                                            // edit canceled
-                                        },
-                                        scope: this
-                                    },
-                                    'viewchange': {
-                                        fn: function(p, vw, dateInfo) {
-                                            if (this.editWin) {
-                                                this.editWin.hide();
-                                            }
-                                            if (dateInfo) {
-                                                // will be null when switching to the event edit form so ignore
-                                                Ext.getCmp('app-nav-picker').setValue(dateInfo.activeDate);
-                                                this.updateTitle(dateInfo.viewStart, dateInfo.viewEnd);
-                                            }
-                                        },
-                                        scope: this
-                                    },
-                                    'dayclick': {
-                                        fn: function(vw, dt, ad, el) {
-                                            this.showEditWindow({
-                                                StartDate: dt,
-                                                IsAllDay: ad
-                                            }, el);
-                                        },
-                                        scope: this
-                                    },
-                                    'rangeselect': {
-                                        fn: function(win, dates, onComplete) {
-                                            this.showEditWindow(dates);
-                                            this.editWin.on('hide', onComplete, this, {single: true});
-                                        },
-                                        scope: this
-                                    },
-                                    'eventmove': {
-                                        fn: function(vw, rec) {
-                                            var mappings = Ext.calendar.data.EventMappings,
-                                                time = rec.data[mappings.IsAllDay.name] ? '' : ' \\a\\t H:i';
+                                        listeners: {
+                                            'eventclick': {
+                                                fn: function(vw, rec, el) {
+                                                    this.showEditWindow(rec, el);
+                                                },
+                                                scope: this
+                                            },
+                                            'eventover': function(vw, rec, el) {
+                                                //console.log('Entered evt rec='+rec.data.Title+', view='+ vw.id +', el='+el.id);
+                                            },
+                                            'eventout': function(vw, rec, el) {
+                                                //console.log('Leaving evt rec='+rec.data.Title+', view='+ vw.id +', el='+el.id);
+                                            },
+                                            'eventadd': {
+                                                fn: function(cp, rec) {
+                                                    this.showMsg('Event ' + rec.data.Title + ' was added');
+                                                },
+                                                scope: this
+                                            },
+                                            'eventupdate': {
+                                                fn: function(cp, rec) {
+                                                    this.showMsg('Event ' + rec.data.Title + ' was updated');
+                                                },
+                                                scope: this
+                                            },
+                                            'eventcancel': {
+                                                fn: function(cp, rec) {
+                                                    // edit canceled
+                                                },
+                                                scope: this
+                                            },
+                                            'viewchange': {
+                                                fn: function(p, vw, dateInfo) {
+                                                    if (this.editWin) {
+                                                        this.editWin.hide();
+                                                    }
+                                                    if (dateInfo) {
+                                                        // will be null when switching to the event edit form so ignore
+                                                        Ext.getCmp('app-nav-picker').setValue(dateInfo.activeDate);
+                                                        this.updateTitle(dateInfo.viewStart, dateInfo.viewEnd);
+                                                    }
+                                                },
+                                                scope: this
+                                            },
+                                            'dayclick': {
+                                                fn: function(vw, dt, ad, el) {
+                                                    this.showEditWindow({
+                                                        StartDate: dt,
+                                                        IsAllDay: ad
+                                                    }, el);
+                                                },
+                                                scope: this
+                                            },
+                                            'rangeselect': {
+                                                fn: function(win, dates, onComplete) {
+                                                    this.showEditWindow(dates);
+                                                    this.editWin.on('hide', onComplete, this, {single: true});
+                                                },
+                                                scope: this
+                                            },
+                                            'eventmove': {
+                                                fn: function(vw, rec) {
+                                                    var mappings = Ext.calendar.data.EventMappings,
+                                                        time = rec.data[mappings.IsAllDay.name] ? '' : ' \\a\\t H:i';
 
-                                            rec.commit();
+                                                    rec.commit();
 
-                                            this.showMsg('Event ' + rec.data[mappings.Title.name] + ' was moved to ' +
-                                                Ext.Date.format(rec.data[mappings.StartDate.name], ('F jS' + time)));
-                                        },
-                                        scope: this
-                                    },
-                                    'eventresize': {
-                                        fn: function(vw, rec) {
-                                            rec.commit();
-                                            this.showMsg('Event ' + rec.data.Title + ' was updated');
-                                        },
-                                        scope: this
-                                    },
-                                    'eventdelete': {
-                                        fn: function(win, rec) {
-                                            this.eventStore.remove(rec);
-                                            this.showMsg('Event ' + rec.data.Title + ' was deleted');
-                                        },
-                                        scope: this
-                                    },
-                                    'initdrag': {
-                                        fn: function(vw) {
-                                            if (this.editWin && this.editWin.isVisible()) {
-                                                this.editWin.hide();
+                                                    this.showMsg('Event ' + rec.data[mappings.Title.name] + ' was moved to ' +
+                                                        Ext.Date.format(rec.data[mappings.StartDate.name], ('F jS' + time)));
+                                                },
+                                                scope: this
+                                            },
+                                            'eventresize': {
+                                                fn: function(vw, rec) {
+                                                    rec.commit();
+                                                    this.showMsg('Event ' + rec.data.Title + ' was updated');
+                                                },
+                                                scope: this
+                                            },
+                                            'eventdelete': {
+                                                fn: function(win, rec) {
+                                                    this.eventStore.remove(rec);
+                                                    this.showMsg('Event ' + rec.data.Title + ' was deleted');
+                                                },
+                                                scope: this
+                                            },
+                                            'initdrag': {
+                                                fn: function(vw) {
+                                                    if (this.editWin && this.editWin.isVisible()) {
+                                                        this.editWin.hide();
+                                                    }
+                                                },
+                                                scope: this
                                             }
-                                        },
-                                        scope: this
+                                        }
                                     }
-                                }
+                                ]
                             }
                         ]
                     }
