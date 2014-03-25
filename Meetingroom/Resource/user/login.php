2ef4a7d872d1f6dc330444447e83207999d169e5
bogi <bogi@syneforge.com>
2014-03-25 16:05:27 +0200

    fixed logout button; reverted login page

--- Meetingroom/Resource/user/login.php
+++ Meetingroom/Resource/user/login.php
@@ -1,16 +1,30 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Meeting Room 2.0</title>
-</head>
-<body>
 
-    <form action="/user/login" method="POST">
-        <input type="text" name="username">
-        <input type="password" name="password">
-        <input type="submit">
-    </form>
-    <p></p>
-</body>
+    <meta charset="utf-8">
+    <title>Login into Meeting Room 2.0</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico?version=1"/>
+    <link rel="stylesheet" type="text/css" href="/js/extjs/resources/css/ext-all-neptune.css"/>
+
+    <script type="text/javascript" src="/js/extjs/ext-all.js"></script>
+    <script type="text/javascript">
+        Ext.Loader.setConfig({
+            enabled: true,
+            paths: {
+                'Ext.calendar': '/js/calendar'
+            }
+        });
+        Ext.require([
+            'Ext.calendar.Login'
+        ]);
+        Ext.onReady(function() {
+            // launch the app
+            Ext.create('Ext.calendar.Login');
+        });
+    </script>
+
+</head>
+<body></body>
 </html>
